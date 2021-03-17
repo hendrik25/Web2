a314d5d42a73dd18a04e63b1a03b012161978325
Muhamad Hendrik Kurniawan <79891592+hendrik25@users.noreply.github.com>
2021-03-17 22:27:06 +0700

    Add files via upload

---
+++ Pert7_latihan7.php
@@ -0,0 +1,16 @@
+<htnl>
+<head>
+<title> Penggunaan Split </title>
+</head>
+<body>
+<?php
+$tanggal = "17-05-2010";
+list($hari, $bulan, $tahun) = explode("-", $tanggal);
+echo "Hari = $hari";
+echo "<br />";
+echo "Bulan = $bulan";
+echo "<br />";
+echo "Tahun = $tahun";
+?>
+</body>
+</html>
---
+++ Pert7_latihan9.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Penggunaan In Array</title>
+</head>
+<body>
+<?php
+	$program = array("HTML", "PHP", "CSS", "JavaScript");
+	print_r($program);
+	$cari = "HTML";
+	echo "<br />";
+	echo "<br />";
+	if (in_array($cari, $program)) 
+	{
+		echo "Program Basis Web $cari ada di dalam array";
+	} 
+	else 
+	{
+		echo "Program Basis Web $cari tidak ada di dalam array";
+	}
+?>
+</body>
+</html>
