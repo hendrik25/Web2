fb46602ed0465c8c8fe9920f2a8a52487bf623a3
Muhamad Hendrik Kurniawan <79891592+hendrik25@users.noreply.github.com>
2021-03-25 17:43:18 +0700

    Add files via upload

---
+++ Pert13_koneksi.php
@@ -0,0 +1,5 @@
+<?php
+$con = mysqli_connect("localhost","root","");
+$dbname = "artikel_db";
+mysqli_select_db($con, $dbname);
+?>
---
+++ Pert13_proses.php
@@ -0,0 +1,24 @@
+<?php
+//include ("Pert13_koneksi.php");
+$con = mysqli_connect("localhost", "root", "", "artikel_db");
+$id = $_POST['id'];
+$judul= $_POST['title'];
+$penulis = $_POST['author'];
+$lead = $_POST['abstraksi'];
+$isi = $_POST['content'];
+$time=date("d M Y, H:i");
+
+$lead = str_replace("\r\n","<br>",$lead);
+$content = str_replace("\r\n","<br>",$isi);
+
+$query = "insert into articles (id,judul,penulis,lead,content,waktu)
+	values('$id','$judul','$penulis','$lead','$isi','$time')";
+$result = mysqli_query($con, $query);
+if($result){
+	echo "<h3 align=center>Proses penambahan artikel berhasil</h3>";
+	echo "<a href=\"Pert13_tampil.php\">List</A>";
+}
+else{
+	echo "<h2 align=center>Proses penambahan artikel tidak berhasil</h2>";
+}
+?>
